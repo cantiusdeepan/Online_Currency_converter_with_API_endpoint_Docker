0370de85444ddd79a62242a1f104192d063116e8
cantius <cantiusdeepan@gmail.com>
2020-02-05 03:13:00 +0100

    Added error handling for missing reference date

--- app.py
+++ app.py
@@ -45,13 +45,17 @@ def return_dest_curr_value():
         ref_date = datetime.strptime(input_date, '%Y-%m-%d')
         now = datetime.now()
         if not (now - timedelta(days=90) <= ref_date <= now):
-            return ('The reference date should be in the past '
+            return ('The reference date should be provided and in the past '
                     '90 days')
     except ValueError as ve:
         return ("Date value passed should be in YYYY-mm-dd format:", ve)
+    # To make sure missing zeros don't cause issues with date format matchng
+    # in XML.
+    input_date = str(ref_date.date())
 
     if (input_amount <= 0.0):
-        return ('Input amount should be a positive integer or float value')
+        return ('Input amount should be provided and be a positive integer or '
+                'float value')
 
     if input_src_curr == 'EU' or input_dest_curr == 'EU':
         return ("Source and destination currencies need to be provided in "
